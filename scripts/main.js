e2da3816366d2bb46d049ebb1f2f05e8ea0556d5
DonSalvadori <csalvi42@gmail.com>
2016-09-03 14:16:18 -0700

    started layout of main app component

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var App = React.createClass({
+
+  render: function(){
+    return(
+      <div className="catch-of-the-day">
+        <div className="menu">
+            <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+});
+
+var Header = React.createClass({
+
+  render: function(){
+    return(
+      <p>Header</p>
+    )
+  }
+});
+
+var Inventory = React.createClass({
+
+  render: function(){
+    return(
+      <p>Inventory</p>
+    )
+  }
+});
+
+
+var Order = React.createClass({
+
+  render: function(){
+    return(
+      <p>Order</p>
+    )
+  }
+});
+
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    return (
+      <form className='store-selector'>
+        <h2>Please Enter A Store</h2>
+        <input type="text" ref='storeId' required />
+        <input type="submit" />
+      </form>
+    )
+  }
+});
+
+
+ReactDOM.render(<App/>, document.querySelector('#main'));
